42281ebc9cf05db083921955fb12cd83f6d464b3
tarikyavuzca <tarikyavuzca@gmail.com>
2023-03-15 13:43:11 -0400

    route table, igw created and configured

--- terraform-aws-1/main.tf
+++ terraform-aws-1/main.tf
@@ -26,3 +26,24 @@ resource "aws_internet_gateway" "mtc_igw" {
     Name = "dev-igw"
   }
 }
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mtc_igw.id
+}
+
+
+resource "aws_route_table_association" "mtc_public_association" {
+  subnet_id      = aws_subnet.mtc_public_subnet.id
+  route_table_id = aws_route_table.mtc_public_rt.id
+
+}
