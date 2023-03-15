4b71e1e70f5f40d63ba5fdedcc2e25f3deb27d21
tarikyavuzca <tarikyavuzca@gmail.com>
2023-03-15 13:32:30 -0400

    internet gateway created

--- terraform-aws-1/main.tf
+++ terraform-aws-1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "mtc_vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "dev"
@@ -9,12 +9,20 @@ resource "aws_vpc" "mtc_vpc" {
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
-  vpc_id = aws_vpc.mtc_vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "mtc_igw" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev-igw"
   }
 }
